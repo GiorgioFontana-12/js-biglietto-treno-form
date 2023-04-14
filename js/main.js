18b0e24442ec045005b9daa9a4f761b4d57bf77a
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-04-14 17:16:29 +0200

    fix js pt3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ let nomeCognome = (document.getElementById("nomeCognome").value)
 let kmPercorsi = parseInt(document.getElementById("km").value);
 let eta = (document.getElementById("eta").value)
 
-var costoTotale   = (kmPercorsi * 0.21  )
 
 const form = document.getElementById("form");
 form.addEventListener("submit", function(event) {
@@ -12,17 +11,21 @@ form.addEventListener("submit", function(event) {
     let nomeCognome = (document.getElementById("nomeCognome").value)
     let kmPercorsi = parseInt(document.getElementById("km").value);
     let eta = (document.getElementById("eta").value)
+    var costoTotale   = (kmPercorsi * 0.21  )
     if (eta == "Minorenne (-18)") {
         let scontoMinori  = (costoTotale * 0.2  )
         let prezzoMinori  = (costoTotale - scontoMinori )
-        // alert ("il costo del tuo biglietto è: " + prezzoMinori.toFixed(2)  + "€" )    
+        alert ("il costo del tuo biglietto è: " + prezzoMinori.toFixed(2)  + "€" )    
     } else if (eta == "Senior (+60)") {
         let scontoAnziani = (costoTotale * 0.40 )
         let prezzoAnziani = (costoTotale - scontoAnziani)
-        // alert ("il costo del tuo biglietto è: " + prezzoAnziani.toFixed(2) + "€" )  
+        alert ("il costo del tuo biglietto è: " + prezzoAnziani.toFixed(2) + "€" )  
+    } else if (eta == "Adulto") {
+        alert ("il costo del tuo biglietto è: " + costoTotale.toFixed(2)   + "€" )
     } else {
-        // alert ("il costo del tuo biglietto è: " + costoTotale.toFixed(2)   + "€" )
+        alert ("Inserire fascia d'età")
     }
+    
 });
 
 
